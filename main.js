d667f737ad0b4ecc5db503c045e679d8feeec3e4
laylaleaks <jonnyecht06@gmail.com>
2024-04-10 00:38:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const fs = require('fs');
 const axios = require('axios');
-const { stringify } = require('querystring');
+const { WebhookClient, EmbedBuilder } = require('discord.js');
+const { twitterClient } = require("./twitterClient.js");
 const endpoint = "https://fortnitecentral.genxgames.gg/api/v1/mappings";
 let data = [];
 
@@ -18,11 +20,43 @@ const checkForUpdate = async () => {
   loadData();
   try {
     const res = await axios.get(endpoint);
-    const currentFileNames = data.map(item => item.fileName);
     const newFileNames = res.data.map(item => item.fileName);
 
-    if (currentFileNames.length!== newFileNames.length ||!currentFileNames.every(fileName => newFileNames.includes(fileName))) {
-      console.log("\u001b[32m[%s] NEW MAPPING FOUND\u001b[0m", res.data[0].fileName);
+    const newMappings = newFileNames.filter(fileName => !data.some(item => item.fileName === fileName));
+
+    if (newMappings.length > 0) {
+      console.log("\u001b[32m[%s] NEW MAPPING FOUND\u001b[0m", newMappings[0]);
+
+      // Create Discord embed
+      const embed = new EmbedBuilder()
+        .setTitle('New Mappings Found!')
+        .addFields(
+          {name: 'Version:', value: res.data.find(item => item.fileName === newMappings[0]).fileName},
+          {name: 'Uploaded:', value: res.data.find(item => item.fileName === newMappings[0]).uploaded},
+          {name: 'Hash:', value: res.data.find(item => item.fileName === newMappings[0]).hash},
+          {name: 'URL:', value: res.data.find(item => item.fileName === newMappings[0]).url}
+        )
+        .setTimestamp()
+        .setFooter({ text: 'made by @layla_leaks'});
+
+      const webhookClient = new WebhookClient({ url: '<DISCORD_WEBHOOK_URL>' });
+      const roleIdToPing = "<ROLE_ID>";
+      webhookClient.send({
+        content: `<@&${roleIdToPing}>`,
+        embeds: [embed]
+      });
+
+      // Post to Twitter
+      const tweetText = `Neue Mappings!\n\n${newMappings[0]}`;
+      const tweet = async () => {
+        try {
+          await twitterClient.v2.tweet(tweetText);
+        } catch (e) {
+          console.log(e)
+        }
+      }
+
+      tweet();
     } else {
       console.log("\u001b[31mNO NEW MAPPING\u001b[0m");
     }
